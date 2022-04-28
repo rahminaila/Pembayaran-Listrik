0a366b3c78d5a8dc95198b4fe1cbcfacc7a32598
Rahmi Nailatunisrina <rahminisrinanaila@gmail.com>
2022-04-28 09:36:14 +0700

    folder masih kosong

---
+++ src/main/java/com/prodemy/pembayaran/listrik/PembayaranListrikApplication.java
@@ -0,0 +1,13 @@
+package com.prodemy.pembayaran.listrik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PembayaranListrikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PembayaranListrikApplication.class, args);
+	}
+
+}
